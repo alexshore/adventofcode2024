50788358a68c62f8f4b6920ffd9ca2b9da93d9fd
Alex Shore <alex@shore.me.uk>
2024-12-01 12:34:08 +0000

    added template

---
+++ template/src/bin/part_one.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input = include_str!("input.txt").trim_end();
+    let _output = dbg!(part_one(input));
+}
+
+fn part_one(input: &str) -> String {
+    todo!()
+}
---
+++ template/src/bin/part_two.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input = include_str!("input.txt").trim_end();
+    let _output = dbg!(part_two(input));
+}
+
+fn part_two(input: &str) -> String {
+    todo!()
+}
