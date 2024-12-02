5b9759f0b68661703db27d5062f731391cc57a80
Alex Shore <alex@shore.me.uk>
2024-12-02 21:15:04 +0000

    day02 finished :)

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,3 @@
-use std::i32;
-
 fn main() {
     let input = include_str!("../input.txt").trim_end();
     println!("{}", part_one(input));
@@ -20,36 +18,54 @@ fn parse_input(input: &str) -> Vec<Vec<i32>> {
 enum ReportState {
     Increasing,
     Decreasing,
-    Unseen,
+    Unknown,
 }
 
-fn part_one(input: &str) -> String {
+fn is_report_safe(report: &[i32]) -> bool {
+    let mut state = ReportState::Unknown;
+    for i in 0..report.len() - 1 {
+        match i32::abs_diff(report[i], report[i + 1]) {
+            1..=3 => (),
+            _ => return false,
+        }
+
+        match &state {
+            ReportState::Unknown if report[i] < report[i + 1] => state = ReportState::Increasing,
+            ReportState::Unknown if report[i] > report[i + 1] => state = ReportState::Decreasing,
+            ReportState::Decreasing if report[i] < report[i + 1] => return false,
+            ReportState::Increasing if report[i] > report[i + 1] => return false,
+            _ => (),
+        }
+    }
+    true
+}
+
+fn part_one(input: &str) -> usize {
+    let reports = parse_input(input);
+
+    reports
+        .iter()
+        .filter(|&report| is_report_safe(report))
+        .count()
+}
+
+fn part_two(input: &str) -> usize {
     let reports = parse_input(input);
 
     reports
         .iter()
         .filter(|&report| {
-            let mut state = ReportState::Unseen;
-            for i in 0..report.len() - 1 {
-                match i32::abs_diff(report[i], report[i + 1]) {
-                    1..=3 => (),
-                    _ => return false,
+            // if report isn't safe by default then we'll check with the "Problem Dampener"
+            if !is_report_safe(report) {
+                for i in 0..report.len() {
+                    // exclude each item in turn to see if there is still a safe configuration
+                    if is_report_safe(&[&report[..i], &report[i + 1..]].concat()) {
+                        return true;
+                    }
                 }
-
-                match (&state, report[i] - report[i + 1]) {
-                    (ReportState::Unseen, diff) if diff < 0 => state = ReportState::Increasing,
-                    (ReportState::Unseen, diff) if diff > 0 => state = ReportState::Decreasing,
-                    (ReportState::Decreasing, diff) if diff < 0 => return false,
-                    (ReportState::Increasing, diff) if diff > 0 => return false,
-                    _ => (),
-                }
+                return false;
             }
             true
         })
         .count()
-        .to_string()
-}
-
-fn part_two(input: &str) -> String {
-    todo!()
 }
